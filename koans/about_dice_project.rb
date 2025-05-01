97d0d96b842064c56b8e816fea613985ab639240
yuliya-grosheva <gros@sfedu.ru>
2025-05-01 19:19:24 +0300

    Changes about_dice_project.rb

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -5,6 +5,17 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # class DiceSet
 #   code ...
 # end
+class DiceSet
+  attr_reader :values
+
+  def initialize
+    @values = []
+  end
+
+  def roll(num_dice)
+    @values = Array.new(num_dice) { rand(1..6) }
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
